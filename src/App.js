aa0b9ec11b77f84c6ad5ead8a22deeca6048c3e6
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-27 16:46:02 -0300

    :construction: build: menu e menuActive

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
 
 const App = () => {
+
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
@@ -16,6 +19,24 @@ const App = () => {
                 <FiSettings/>
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              Sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              Sidebar w-0
+            </div>
+          )}
+          <div className={
+            activeMenu
+              ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full'
+              : 'bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2'
+          }>
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              Navbar
+            </div>
           </div>
         </div>
       </BrowserRouter>
